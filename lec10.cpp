63c6fb2c142660fd7f526d86f62df13c2291a154
fleuren <91404708+fleuren@users.noreply.github.com>
2021-12-09 18:35:57 +0400

    Add files via upload

---
+++ lec10.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+//grade avg calculator	
+/*
+	cout << "Please input the marks of 5 subjects" << endl;
+	double a, b, c, d, e;
+	cin >> a >> b >> c >> d >> e;
+
+if (a >= 0 && a <= 100,b >= 0 && b <= 100,c >= 0 && c <= 100,d >= 0 && d <= 100,e >= 0 && e <= 100){
+	double sum = a + b + c + d + e;
+	double avg = sum/5;
+	cout << sum << avg;	
+}else{
+	cout << "Invalid input";
+}
+*/	
+//next task: age identifier
+/*
+int age;
+cin >> age;
+
+if (age >= 10 && age <= 12){
+	cout << "preteen";
+}else if (age >= 13 && age <= 19){
+	cout << "teen";
+}else if (age >= 20 && age <= 29){
+	cout << "twenties";
+}else if (age >= 30 && age <= 39){
+	cout << "thirties";
+}else if (age >= 40 && age <= 49){
+	cout << "forties";
+}else if (age >= 50 && age <= 59){
+	cout << "fifties";
+}else if (age >= 60){
+	cout << "sixties or above";
+}else{
+	cout << "Invalid input";
+}
+*/
+	return 0;
+}
